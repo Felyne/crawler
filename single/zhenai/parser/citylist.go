39aedb23dd22a57d498a412b01c59309fb95eb98
Felyne <chenwd_135@qq.com>
2019-09-22 00:29:31 +0800

    update single parser city count limit

--- single/zhenai/parser/citylist.go
+++ single/zhenai/parser/citylist.go
@@ -6,12 +6,13 @@ import (
 )
 
 var cityListRe = regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[a-z0-9]+)"[^>]*>([^<]+)</a>`)
+const limit = 5
 
 //根据城市列表页面解析各个城市的url
 func ParseCityList(contents []byte) engine.ParseResult {
 	matches := cityListRe.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
-	limit := 2
+	n := limit
 	//m[1]是城市对应的url，m[2]是城市名
 	for _, m := range matches {
 		result.Items = append(result.Items, "City: "+string(m[2]))
@@ -19,8 +20,8 @@ func ParseCityList(contents []byte) engine.ParseResult {
 			Url:        string(m[1]),
 			ParserFunc: ParseCity, //该Url对应的的解析器
 		})
-		limit--
-		if limit == 0 {
+		n--
+		if n == 0 {
 			break
 		}
 	}
